ab512f929f2f8729e016866b9f5cab123c928a4f
MishraAbhishek07 <davmishra96@gmail.com>
2024-11-15 15:18:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function firstWord(s) {
   // your code here
+	let fw=s.split(" ")
+	return fw[0]
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+// const s = prompt("Enter String:");
 alert(firstWord(s));
